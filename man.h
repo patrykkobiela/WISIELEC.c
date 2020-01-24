7b999586239f781f1c7c3679ee3b0bf3bbf21862
patrykkobiela <57020326+patrykkobiela@users.noreply.github.com>
2020-01-24 19:15:27 +0100

    Update man.h

--- man.h
+++ man.h
@@ -4,3 +4,14 @@
 #include <ctype.h>
 #include <termio.h>
 #include <time.h>
+
+void title(void)   
+{
+      printf("\t*******************************************************\n");
+      printf("\t*\x1b[40m\x1b[32m#     #     #  #   # #   #  ####  #     ####      # \x1b[0m *\n");
+      printf("\t*\x1b[40m\x1b[32m#    # #    #  #  #      #  #     #     #      #    \x1b[0m *\n");
+      printf("\t*\x1b[40m\x1b[32m #   # #   #   #   # #   #  ####  #     ####  #     \x1b[0m *\n");
+      printf("\t*\x1b[40m\x1b[32m #  #   #  #   #      #  #  #     #     #      #    \x1b[0m *\n");
+      printf("\t*\x1b[40m\x1b[32m  #      #     #   # #   #  ####  ####  ####      # \x1b[0m *\n");
+      printf("\t*******************************************************\n\n");
+}
