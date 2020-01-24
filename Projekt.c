0a46eaf805dcee1094e7913c63863eb8f1aa7fe5
patrykkobiela <57020326+patrykkobiela@users.noreply.github.com>
2020-01-24 19:24:27 +0100

    Update Projekt.c

--- Projekt.c
+++ Projekt.c
@@ -1,1 +1,11 @@
 #include "man.h"
+
+void menu(void){
+    int *id;
+    printf("Menu programu\n\n");
+    printf("Nowa gra (wybierz 1)\n");
+    printf("Poprzedni stan gry (wybierz 2)\n");
+    printf("Język gry (wybierz 3)\n");
+    printf("Zamknij program (wybierz 4)\n");
+    scanf("%d", *id);
+}
